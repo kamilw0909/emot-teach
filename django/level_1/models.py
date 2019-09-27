426a900cb269e60948873b4f497da541884226cc
kamilw0909 <kwardach88@gmail.com>
2019-09-27 11:57:01 +0200

    Add emotion model

--- django/level_1/models.py
+++ django/level_1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Emotion(models.Model):
+    name = models.CharField(max_length=30)
+    img = models.ImageField()
+
+    def __str__(self):
+        return self.name
